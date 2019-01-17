457a20cdd081e219fb6fffdea723fa600682f122
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2019-01-17 16:40:23 +0800

    feat: Setup storybook library to facade file

--- storybook/addons.js
+++ storybook/addons.js
@@ -1,4 +1,8 @@
+// 每次yarn add新的storybook插件，都要來這註冊
+import 'storybook-addon-specifications/register';
 import '@storybook/addon-actions/register';
+import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-jest/register';
 import '@storybook/addon-options/register';
+
